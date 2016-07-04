45fcfb5b04bf71858d8aefe71b1f9ab1b3151838
Hung Q. Nguyen <hnguyenworkstation@gmail.com>
2016-07-04 07:50:56 -0400

    Basic Design of default application layout

---
+++ BottleChat/app/src/main/java/com/onlineteammakers/bottlechat/Fragments/ProfileFragment.java
@@ -0,0 +1,27 @@
+package com.onlineteammakers.bottlechat.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.onlineteammakers.bottlechat.R;
+
+public class ProfileFragment extends Fragment {
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.profile_fragment, container, false);
+    }
+}
--- BottleChat/app/src/main/java/com/onlineteammakers/bottlechat/MainActivity.java
+++ BottleChat/app/src/main/java/com/onlineteammakers/bottlechat/MainActivity.java
@@ -1,14 +1,110 @@
 package com.onlineteammakers.bottlechat;
 
-import android.app.Activity;
+import android.content.Context;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends Activity {
+import com.onlineteammakers.bottlechat.Fragments.CommunicateFragment;
+import com.onlineteammakers.bottlechat.Fragments.ExploreFragment;
+import com.onlineteammakers.bottlechat.Fragments.HomeFragment;
+import com.onlineteammakers.bottlechat.Fragments.ProfileFragment;
+import com.onlineteammakers.bottlechat.Fragments.SeekAroundFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static TabLayout tabLayout;
+    private Toolbar toolbar;
+
+    private static int[] _unSelTabIcons = {
+            R.drawable.ic_home_black,
+            R.drawable.ic_update_black,
+            R.drawable.ic_track_changes_black,
+            R.drawable.ic_explore_black,
+            R.drawable.ic_account_circle_black
+    };
+
+    private static int[] _selTabIcons = {
+            R.drawable.ic_home_white,
+            R.drawable.ic_update_white,
+            R.drawable.ic_track_changes_white,
+            R.drawable.ic_explore_white,
+            R.drawable.ic_account_circle_white
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_content);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        assert tabLayout != null;
+        tabLayout.setupWithViewPager(viewPager);
+
+        setupTabIcons();
+    }
+
+    private void setupTabIcons() {
+        for (int position = 0; position < _selTabIcons.length; position++) {
+            tabLayout.getTabAt(position).setIcon(_selTabIcons[position]);
+        }
+    }
+
+    private void setupSelectedTab(int position) {
+        tabLayout.getTabAt(position).setIcon(_selTabIcons[position]);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this.getBaseContext(), getSupportFragmentManager());
+        adapter.addFragment(new HomeFragment(), "Home");
+        adapter.addFragment(new CommunicateFragment(), "Chat");
+        adapter.addFragment(new SeekAroundFragment(), "Seek");
+        adapter.addFragment(new ExploreFragment(), "Explore");
+        adapter.addFragment(new ProfileFragment(), "Me");
+
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitle = new ArrayList<>();
+
+        public Context mContext;
+
+        public ViewPagerAdapter(Context mContext,FragmentManager manager) {
+            super(manager);
+            this.mContext = mContext;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment,String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitle.add(title);
+        }
     }
 }
